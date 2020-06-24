c20817b366829f3022d5f8c59fdd123415cd4992
TheBitsDontByte <chris.chase.andrews@gmail.com>
2020-06-24 17:41:01 -0600

    rename, final changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import AppointmentScheduler from "./AppointmentScheduler";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<AppointmentScheduler />, document.getElementById("root"));
